798473465cef01004832e2f86168cfb22077b7b0
Steve Murphy <murf@e-tools.com>
2003-07-22 02:03:04 +0000

    Nope. still not right. if_addr comes up null, and a crash results. I brought back a test for "local" and 127.0.0.1... perhaps this situation needs a review, but 1.15 did work with local interfaces...

--- sendmail-milter/relaydelay/relaydelay.c
+++ sendmail-milter/relaydelay/relaydelay.c
@@ -854,7 +854,7 @@ sfsistat envrcpt_callback(SMFICTX *ctx, char **argv)
 	}
 
         /* Check to see if the mail is looped back on a local interface and skip checks if so */
-	if( strcmp(if_addr,relay_ip) == 0 )
+	if( (if_addr && strcmp(if_addr,relay_ip) == 0) || !strcmp(mail_mailer,"local") || !strcmp(relay_ip,"127.0.0.1") )
 	{
 		/* we aren't using an smtp-like mailer, so bypass checks */
 		writelog(1,"  Mail delivery is sent from a local interface.  Skipping checks (%s).  Skipping checks.\n",
